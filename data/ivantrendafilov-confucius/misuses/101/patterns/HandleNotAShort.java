c9a5e1e99e22c4c335b2a1bfb6ba70883edbcf18
M8is <m8is@live.de>
2017-09-26 09:19:47 +0200

    Fix ivantrendafilov-confucius misuse locations

---
+++ data/ivantrendafilov-confucius/misuses/101/patterns/HandleNotAShort.java
@@ -0,0 +1,10 @@
+
+class HandleNotANumber {
+  long pattern(String s) {
+    try {
+        return Short.parseShort(s);
+    } catch (NumberFormatException e) {
+      throw new NumberFormatException(String.format("Input string [%s] is not a parseable short", s));
+    }
+  }
+}
