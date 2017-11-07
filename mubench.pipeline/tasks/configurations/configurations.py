a5998c76eb5bf3ffd2bbb01f6b02f918b36de4f3
Mattis Manfred Kämmerer <mattis.kaemmerer@gmx.de>
2017-11-07 10:47:30 +0100

    Refactor task configuration factory

--- mubench.pipeline/tasks/configurations/configurations.py
+++ mubench.pipeline/tasks/configurations/configurations.py
@@ -1,23 +1,30 @@
 from typing import List
 
 
-class Configuration(list):
-    def __init__(self):
+
+class TaskConfiguration(list):
+    def __init__(self, paths: MubenchPaths, config):
         super().__init__()
-        self.extend(self._tasks())
+        self.extend(self._tasks(paths, config))
 
-    def mode(self) -> str:
+    @staticmethod
+    def mode() -> str:
         raise NotImplementedError
 
-    def _tasks(self) -> List:
+    def _tasks(self, paths: MubenchPaths, config) -> List:
         raise NotImplementedError
 
 
-def get_configuration(mode: str) -> Configuration:
-    requested_configurations = [conf() for conf in Configuration.__subclasses__() if conf().mode() == mode]
+def get_task_configuration(paths: MubenchPaths, config) -> TaskConfiguration:
+    mode = config.task
+    if config.publish_task:
+        mode += " " + config.publish_task
+
+    requested_configurations = [task_config(paths, config) for task_config in
+                                TaskConfiguration.__subclasses__() if task_config.mode() == mode]
     if len(requested_configurations) > 1:
-        raise ValueError("Multiple configurations for mode {}".format(mode))
+        raise ValueError("Multiple configurations for {}".format(mode))
     if len(requested_configurations) == 0:
-        raise ValueError("No configuration available for mode {}".format(mode))
+        raise ValueError("No configuration available for {}".format(mode))
 
     return requested_configurations[0]
--- mubench.pipeline/tests/tasks/configurations/test_configurations.py
+++ mubench.pipeline/tests/tasks/configurations/test_configurations.py
@@ -2,40 +2,50 @@ from typing import List
 
 from nose.tools import assert_is_instance, assert_raises
 
-from tasks.configurations.configurations import Configuration, get_configuration
+from tasks.configurations.configurations import TaskConfiguration, get_task_configuration
+from tasks.configurations.mubench_paths import MubenchPaths
 
 
-class TestConfigurations:
+class ConfigDummy:
+    def __init__(self, mode: str):
+        self.task = mode
+        self.publish_task = ""
+
+
+class TestTaskConfigurations:
     def test_get_configuration(self):
-        configuration = get_configuration("-test-")
-        assert_is_instance(configuration, ConfigurationTestImpl)
+        configuration = get_task_configuration(MubenchPaths, ConfigDummy("-test-"))
+        assert_is_instance(configuration, TaskConfigurationTestImpl)
 
     def test_duplicate_raises_value_error(self):
-        assert_raises(ValueError, get_configuration, "-duplicate-")
+        assert_raises(ValueError, get_task_configuration, MubenchPaths, ConfigDummy("-duplicate-"))
 
     def test_no_available_configuration_raises_value_error(self):
-        assert_raises(ValueError, get_configuration, "-unavailable-")
+        assert_raises(ValueError, get_task_configuration, MubenchPaths, ConfigDummy("-unavailable-"))
 
 
-class ConfigurationTestImpl(Configuration):
-    def mode(self) -> str:
+class TaskConfigurationTestImpl(TaskConfiguration):
+    @staticmethod
+    def mode() -> str:
         return "-test-"
 
-    def _tasks(self) -> List:
+    def _tasks(self, paths: MubenchPaths, config) -> List:
         return []
 
 
-class DuplicateConfiguration(Configuration):
-    def _tasks(self) -> List:
+class DuplicateTaskConfiguration(TaskConfiguration):
+    def _tasks(self, paths: MubenchPaths, config) -> List:
         return []
 
-    def mode(self) -> str:
+    @staticmethod
+    def mode() -> str:
         return "-duplicate-"
 
 
-class DuplicateConfiguration2(Configuration):
-    def _tasks(self) -> List:
+class DuplicateTaskConfiguration2(TaskConfiguration):
+    def _tasks(self, paths: MubenchPaths, config) -> List:
         return []
 
-    def mode(self) -> str:
+    @staticmethod
+    def mode() -> str:
         return "-duplicate-"
